4e6365ea781f74e3fd1040abcc84ad2c34d5fad1
Eurico <eurico.manel@gmail.com>
2017-05-01 20:48:58 +0100

    k

--- JavaCoding/Video_Poker/src/stuff/Main.java
+++ JavaCoding/Video_Poker/src/stuff/Main.java
@@ -1,11 +1,14 @@
 package stuff;
+import game.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, brace yourselves...");
-		System.out.println("poo is coming!!"); 
+		Credit credit = new Credit(4);
+		System.out.println(credit.getActual_credit());
+		credit.bet(4);
+		System.out.println("Inicialmente tinhas este crédito: " + credit.getInitial_credit() + ".");
+		System.out.println("Agora tens este crédito: " + credit.getActual_credit() + ".");
 	}
 
 }
