af62ef524af431088791937e7049910f82533087
luis-frodrigues <luis.ferreira.rodrigues@tecnico.ulisboa.pt>
2017-05-03 21:55:16 +0100

    dunno what im doing

--- JavaCoding/Video_Poker/src/game/Interactive.java
+++ JavaCoding/Video_Poker/src/game/Interactive.java
@@ -8,9 +8,28 @@ public class Interactive extends Game{
 	}
 
 	@Override
+	public
 	void init() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public String Process1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String Process2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String Process3() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
