a3459583ad471c12aeddeceff15001166233d672
Eurico <eurico.manel@gmail.com>
2017-05-03 18:53:10 +0100

    Process3 underway

--- JavaCoding/Video_Poker/src/game/Game.java
+++ JavaCoding/Video_Poker/src/game/Game.java
@@ -8,47 +8,75 @@ public abstract class Game{
 		
 	}
 	
+	static boolean verifyHold(String cmd3){
+		if (cmd3.length()==0){
+			return(true);
+		}else{
+			if (cmd3.charAt(0)!='h'){
+				return(true);
+			}else{
+				
+			}
+			return(true);
+		}
+	}
+	
 	public void init(){
 		
 		String cmd1="ABC";
+		String cmd2="ABC";
+		String cmd3="ABC";
 		String[] cmdaux;
 		int i=0;
 		boolean isInteger=true;
 		//Assume-se que receberá no máximo dois caracteres;
 		cmd1=Process1();
 		
-		while((cmd1!="b")&&(cmd1!="b 1")&&(cmd1!="b 2")&&(cmd1!="b 3")&&(cmd1!="b 4")&&(cmd1!="b 5")&&(cmd1!="q")&&(cmd1!="$")&&(cmd1!="s")){
-			if(cmd1.length()<=1){
-				System.out.println(cmd1 +": illegal command1"); // caso em que é apenas 1 caracter e não corresponde a $,b ou q
-			}
-			else{
-				if((cmd1.charAt(1)==' ')&&(cmd1.length()>2)){ //se o segundo caracter não for 1 espaço = illegal command (não existem comandos com mais q 1 char)
-					cmdaux=cmd1.split(" ");
-					if (cmdaux.length<3){ //não existem comandos com 3 strings
-						if(cmdaux[0].charAt(0)=='b'){
-							while(i<cmdaux[1].length()&&(isInteger)){
-								if((cmdaux[1].charAt(i)<'0')||(cmdaux[1].charAt(i)>'9')){ //
-									isInteger=false;
+		while((cmd1!="b")&&(cmd1!="b 1")&&(cmd1!="b 2")&&(cmd1!="b 3")&&(cmd1!="b 4")&&(cmd1!="b 5")){
+			switch(cmd1){
+			case "s":
+				//Apresenta estatísticas
+			break;
+			case "$":
+				System.out.println("player's credit is "+credit.getActual_credit());//Apresenta o crédito atual
+			break;
+			case "q":
+				System.exit(0);//Sai do jogo;
+			break;
+			default:
+				if(cmd1.length()<=1){
+					System.out.println(cmd1 +": illegal command1"); // caso em que é apenas 1 caracter e não corresponde a $,b ou q
+				}
+				else{
+					if((cmd1.charAt(1)==' ')&&(cmd1.length()>2)){ //se o segundo caracter não for 1 espaço = illegal command (não existem comandos com mais q 1 char)
+						cmdaux=cmd1.split(" ");
+						if (cmdaux.length<3){ //não existem comandos com 3 strings
+							if(cmdaux[0].charAt(0)=='b'){
+								while(i<cmdaux[1].length()&&(isInteger)){
+									if((cmdaux[1].charAt(i)<'0')||(cmdaux[1].charAt(i)>'9')){ //
+										isInteger=false;
+									}
+									i++;
 								}
-								i++;
-							}
-							if(isInteger){// falta o caso de numeros negativos e floats: "b 1.2323"
-								System.out.println("b: illegal amount");
+								if(isInteger){// falta o caso de numeros negativos e floats: "b 1.2323"
+									System.out.println("b: illegal amount");
+								}else{
+									System.out.println(cmd1 +": illegal command2");
+								}
 							}else{
-								System.out.println(cmd1 +": illegal command2");
+								System.out.println(cmd1 +": illegal command3");
 							}
 						}else{
-							System.out.println(cmd1 +": illegal command3");
+							System.out.println(cmd1 +": illegal command4");
 						}
 					}else{
-						System.out.println(cmd1 +": illegal command4");
+					System.out.println(cmd1 +": illegal command5");
 					}
-				}else{
-				System.out.println(cmd1 +": illegal command5");
 				}
 			}
 			cmd1=Process1();
 		}
+		
 		switch(cmd1) {
 			case "b":
 				credit.bet();
@@ -68,9 +96,14 @@ public abstract class Game{
 			case "b 5":
 				credit.bet(5);
 			break;
-			case "s":
-				//mambos
-			break;
+			default:
+				System.out.println("Error: Command entry is wrong.");// Optional
+		}
+		
+		cmd2=Process2();
+		
+		while(cmd2!="d"){
+			switch(cmd2){
 			case "$":
 				System.out.println("player's credit is "+credit.getActual_credit());//mambos
 			break;
@@ -78,9 +111,29 @@ public abstract class Game{
 				System.exit(0);//leave game;
 			break;
 			default:
-				System.out.println("Error: Command entry is wrong.");// Optional
-		      // Statements
+				System.out.println(cmd2+": illegal command");
+				
+			}
+			cmd2=Process2();
 		}
+		
+		switch(cmd2) {
+			case "d":
+				/*
+						Statements que distribuem as cartas.
+				  */
+				 
+			break;
+			default:
+				System.out.println("Erro fatal");
+		}
+		
+		cmd3= Process3();
+		
+		while(verifyHold(cmd3)){
+			
+		}
+		
 		
 	}
 		
