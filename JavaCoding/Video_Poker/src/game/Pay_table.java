402867c3d6445de2199f13bc12a19e5e7c27b1c6
luis-frodrigues <luis.ferreira.rodrigues@tecnico.ulisboa.pt>
2017-05-04 13:55:49 +0100

    Pay_Table done

--- JavaCoding/Video_Poker/src/game/Pay_table.java
+++ JavaCoding/Video_Poker/src/game/Pay_table.java
@@ -3,27 +3,31 @@ package game;
 import java.util.Hashtable;
 
 abstract class  Pay_table {
-	int elem_on_table=0;
-	int max_pay_types=0;
-	Hashtable<Object, Object> PayTable;
-	Hashtable<Object,Integer>[] aux;
-	
-	@SuppressWarnings("unchecked")
-	Pay_table(int max_pay_types){
+	private int elem_on_table=0;
+	private int max_pay_types=0;
+	protected int max_credits=0;
+	protected Hashtable<Object, Object> PayTable;
+		
+	Pay_table(int max_pay_types, int max_credits){
 		PayTable = new Hashtable<Object, Object>();
-		aux= new Hashtable[max_pay_types];
+		this.max_credits=max_credits;
 		this.max_pay_types=max_pay_types;
 	}
 		
-	void intsertValue(Object OutcomeType, Object Credit, int Value){
-		if((PayTable.containsKey(OutcomeType))&&(elem_on_table>0)){
-			Hashtable<Object, Integer> tmp= (Hashtable<Object, Integer>) PayTable.get(OutcomeType);
-			tmp.put(Credit, Value);
+	@SuppressWarnings("unchecked")
+	void intsertValue(Object HandType, Object Credit, int Value){
+		if((PayTable.containsKey(HandType))&&(elem_on_table>0)){
+			Hashtable<Object, Integer> tmp=  (Hashtable<Object, Integer>) PayTable.get(HandType);
+			if(tmp.size()<max_credits){
+				tmp.put(Credit, Value);
+			}else{
+				System.out.println("Max credits reached");
+			}
 		}else{
 			if(elem_on_table<max_pay_types){
-				aux[elem_on_table]=new Hashtable<Object, Integer>();
-				aux[elem_on_table].put(Credit, Value);
-				PayTable.put(OutcomeType, aux[elem_on_table]);
+				Hashtable<Object, Integer> aux=new Hashtable<Object, Integer>();
+				aux.put(Credit, Value);
+				PayTable.put(HandType, aux);
 				elem_on_table++;
 			}else{
 				System.out.println("Table at full capacity");
@@ -32,22 +36,23 @@ abstract class  Pay_table {
 	}
 	
 	@SuppressWarnings("unchecked")
-	int check_payout(Object OutcomeType, Object Credit){
-		if(PayTable.containsKey(OutcomeType)){
-			Hashtable<Object,Integer> tmp= (Hashtable<Object, Integer>) PayTable.get(OutcomeType);
+	int check_payout(Object HandType, Object Credit){
+		if(PayTable.containsKey(HandType)){
+			Hashtable<Object,Integer> tmp= (Hashtable<Object, Integer>) PayTable.get(HandType);
 			if(tmp.containsKey(Credit)){
 				int i=(int) tmp.get(Credit);
 				return i;
 			}
-			return 1;
+			System.out.println("Credit '"+Credit+"' does not exit for that HandType");
+			return -1;
 		}
-		return 0;
+		return -2;
 	}
 	
 	@SuppressWarnings("unchecked")
-	void removeValue(Object OutcomeType, Object Credit){
-		if(PayTable.containsKey(OutcomeType)){
-			Hashtable<Object, Integer> tmp= (Hashtable<Object, Integer>) PayTable.get(OutcomeType);
+	void removeValue(Object HandType, Object Credit){
+		if(PayTable.containsKey(HandType)){
+			Hashtable<Object, Integer> tmp= (Hashtable<Object, Integer>) PayTable.get(HandType);
 			if(tmp.contains(Credit)){
 				tmp.remove(Credit);
 			}
