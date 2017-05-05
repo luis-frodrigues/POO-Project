55051562753d2a97ab29acd9c8afe8f14250fa20
luis-frodrigues <luis.ferreira.rodrigues@tecnico.ulisboa.pt>
2017-05-05 17:14:14 +0100

    straigth

--- JavaCoding/Video_Poker/src/game/Verify.java
+++ JavaCoding/Video_Poker/src/game/Verify.java
@@ -72,23 +72,52 @@ public interface Verify {
 						if(j==0){
 							flag++;
 							n_cards_left=Math.max(n_cards_left, flag);
-						}
-						for(int k=0; k<j;k++){
-							if(aux[k]==aux[j])
-								flag2=1;
-						}
-						if(flag2==0){
-							flag++;
-							n_cards_left=Math.max(n_cards_left, flag);
+						}else{
+							for(int k=0; k<j;k++){
+								if(aux[k]==aux[j])
+									flag2=1;
+							}
+							if(flag2==0){
+								flag++;
+								n_cards_left=Math.max(n_cards_left, flag);
+							}
 						}
 					}
 				}
 			}
+			straight[0]++; straight[1]++; straight[2]++;
+			straight[3]++;straight[4]++;
 		}
 		return (5-n_cards_left);
 	}
 	static int Flush(Card[] deck ) {
-		return 0;
+		int flag=1,n_cards_left=1, already_verified=0, flag2=0;
+		int[]aux=new int[4];
+		for(int i=0;i<4;i++){
+			flag=1;flag2=0;
+			for(int k=0;k < already_verified;k++){
+				if(i==aux[k]){
+					flag2=1;
+					break;
+				}
+			}
+			if(flag2==1){
+				continue;
+			}
+			for(int j=0;j<4;j++){
+				if(((deck[i].value)/13)==((deck[j].value)/13)){
+					if(i!=j){
+						flag++;
+						n_cards_left=Math.max(flag,n_cards_left);
+						aux[already_verified]=j;
+						already_verified++;
+					}
+				}
+			}
+		}
+		
+		
+		return (5-n_cards_left);
 	}
 	static int FullHouse(Card[] deck ) {
 		return 0;
