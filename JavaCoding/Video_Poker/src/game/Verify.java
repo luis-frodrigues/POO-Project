67aaa88e818afbbfb4228c9af5ab95a40defd5a7
luis-frodrigues <luis.ferreira.rodrigues@tecnico.ulisboa.pt>
2017-05-06 01:46:12 +0100

    estou bebedo

--- JavaCoding/Video_Poker/src/game/Verify.java
+++ JavaCoding/Video_Poker/src/game/Verify.java
@@ -7,7 +7,7 @@ public interface Verify {
 		int flag=0, aux[]=new int[4];
 		for(int i=0; i<5;i++){
 			if(((deck[i].value+1)%13)<=4){
-				aux[flag]=i;
+				aux[flag]=i+1;
 				flag++;
 			}
 		}
@@ -26,7 +26,7 @@ public interface Verify {
 			flag2=0;
 			if(i<1&&flag>0){
 				for(int k=0;k<flag;k++){
-					if(i==aux[k]){
+					if(i==(aux[k]-1)){
 						flag2=1;
 						break;
 					}	
@@ -38,9 +38,9 @@ public interface Verify {
 			for(int j=0; j<i;j++){
 				if(j!=i){
 					if(((deck[j].value)%13)==((deck[i].value)%13)){
-						aux[flag]=j;
+						aux[flag]=j+1;
 						flag++;
-						aux[flag]=i;
+						aux[flag]=i+1;
 						flag++;
 					}
 				}
@@ -67,8 +67,8 @@ public interface Verify {
 					flag++;
 					if(n_cards_left<flag){
 						n_cards_left=flag;
-						aux[n_cards_left]=j;
-						aux[0]=i;
+						aux[n_cards_left]=j+1;
+						aux[0]=i+1;
 					}
 				}
 			}
@@ -79,7 +79,8 @@ public interface Verify {
 		Ret.setPos(aux);
 		return Ret;
 	}
-	
+	//Returns the positions of pair(s) and the number
+	//of cards to fulfill FourOfaKind
 	static RetVerify FourOfaKind(Card[] deck ) {
 		int aux[]= new int[5], flag=0, n_cards_left=0;
 		for(int i=0; i<5;i++){
@@ -89,8 +90,8 @@ public interface Verify {
 					flag++;
 					if(n_cards_left<flag){
 						n_cards_left=flag;
-						aux[n_cards_left]=j;
-						aux[0]=i;
+						aux[n_cards_left]=j+1;
+						aux[0]=i+1;
 					}
 				}
 			}
@@ -99,27 +100,39 @@ public interface Verify {
 		Ret.setPos(aux);
 		return Ret;
 	}
-	static int Straight(Card[] deck ) {
+	
+	//Returns the positions of a straight and the number
+	//of cards to fulfill a straigth
+	static RetVerify Straight(Card[] deck ) {
 		int[] straight={0,1,2,3,4}; 
-		int flag=0,n_cards_left=0, flag2=0;
+		int aux2[]= new int [5];
+		int flag=0,n_cards_left=0, flag2=0,flag3=0;
+		int aux[]=new int[5];
+		int hold[]=new int[5];
 		for(int i=0; i<9;i++){
-			flag=0; flag2=0;
-			int aux[]={-1,-1,-1,-1,-1};
+			flag=0; flag3=0;
 			for(int j=0; j<=4;j++){
+				flag2=1;
 				for(int v=0; v<=4;v++){
 					if(straight[v]==((deck[j].value)%13)){
-						aux[j]=v;
+						aux[flag3]=j;
+						flag3++;
 						if(j==0){
 							flag++;
+							aux2[0]=j+1;
 							n_cards_left=Math.max(n_cards_left, flag);
+							if(i==0)
+								hold[0]=j;
 						}else{
-							for(int k=0; k<j;k++){
-								if(aux[k]==aux[j])
-									flag2=1;
+							for(int k=0; k<=j;k++){
+								if(((deck[k].value)%13)!=((deck[j].value)%13))
+									flag2++;
 							}
-							if(flag2==0){
-								flag++;
-								n_cards_left=Math.max(n_cards_left, flag);
+							if(flag2!=0&&flag2>n_cards_left){
+								n_cards_left=Math.max(n_cards_left, flag2);
+								for(int y=0;y<n_cards_left;y++){
+									hold[y]=aux[y];	
+								}
 							}
 						}
 					}
@@ -127,9 +140,15 @@ public interface Verify {
 			}
 			straight[0]++; straight[1]++; straight[2]++;
 			straight[3]++;straight[4]++;
+			if(n_cards_left==5)
+				break;
 		}
-		return (5-n_cards_left);
+		RetVerify Ret= new RetVerify(n_cards_left);
+		Ret.setPos(hold);
+		Ret.n_ret=5-n_cards_left;
+		return Ret;
 	}
+	
 	static int Flush(Card[] deck ) {
 		int flag=1,n_cards_left=1, already_verified=0, flag2=0;
 		int[]aux=new int[4];
